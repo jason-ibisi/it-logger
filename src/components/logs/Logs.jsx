220476fdd8051308600f4dbf043ee46ca93f4dbe
Jason IBISI <jasonibisi@gmail.com>
2020-01-13 18:36:22 +0100

    Redux: getLogs connect to component

--- src/components/logs/Logs.jsx
+++ src/components/logs/Logs.jsx
@@ -1,37 +1,27 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 import LogItem from './LogItem';
+import { getLogs } from '../../actions/logActions';
 import Preloader from '../layout/Preloader';
 
-const Logs = () => {
-  const [logs, setLogs] = useState([]);
-  const [loading, setLoading] = useState(false);
-
+const Logs = ({ log: { logs, loading }, getLogs }) => {
   useEffect(() => {
     getLogs();
     //eslint-disable-next-line
   }, []);
 
-  const getLogs = async () => {
-    setLoading(true);
-
-    const res = await fetch('/logs');
-    const data = await res.json();
-
-    setLogs(data);
-    setLoading(false);
-  };
-
-  if (loading) {
+  if (loading || logs === null) {
     return <Preloader />;
   }
 
   return (
-    <ul className="collection with-header">
-      <li className="collection-header">
-        <h4 className="center">System Logs</h4>
+    <ul className='collection with-header'>
+      <li className='collection-header'>
+        <h4 className='center'>System Logs</h4>
       </li>
       {!loading && logs.length === 0 ? (
-        <p className="center">No logs to show</p>
+        <p className='center'>No logs to show</p>
       ) : (
         logs.map(log => <LogItem log={log} key={log.id} />)
       )}
@@ -39,4 +29,13 @@ const Logs = () => {
   );
 };
 
-export default Logs;
+Logs.propTypes = {
+  log: PropTypes.object.isRequired,
+  getLogs: PropTypes.func.isRequired
+};
+
+const mapStateToProps = state => ({
+  log: state.log
+});
+
+export default connect(mapStateToProps, { getLogs })(Logs);
