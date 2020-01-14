e949259ba3209921aa5eef56137dec23e3796bec
Jason IBISI <jasonibisi@gmail.com>
2020-01-14 10:01:21 +0100

    Redux: searchLogs action, reducer

--- src/actions/logActions.js
+++ src/actions/logActions.js
@@ -6,7 +6,8 @@ import {
   LOGS_ERROR,
   SET_CURRENT,
   CLEAR_CURRENT,
-  UPDATE_LOG
+  UPDATE_LOG,
+  SEARCH_LOGS
 } from './types';
 
 // Get Logs
@@ -74,6 +75,20 @@ export const updateLog = log => async dispatch => {
     const data = await res.json();
 
     dispatch({ type: UPDATE_LOG, payload: data });
+  } catch (err) {
+    dispatch({ type: LOGS_ERROR, payload: err.response.data });
+  }
+};
+
+// Search Logs
+export const searchLogs = text => async dispatch => {
+  try {
+    setLoading();
+
+    const res = await fetch(`/logs?q=${text}`);
+    const data = await res.json();
+
+    dispatch({ type: SEARCH_LOGS, payload: data });
   } catch (err) {
     dispatch({ type: LOGS_ERROR, payload: err.response.data });
   }
--- src/reducers/logReducer.js
+++ src/reducers/logReducer.js
@@ -6,7 +6,8 @@ import {
   DELETE_LOG,
   UPDATE_LOG,
   SET_CURRENT,
-  CLEAR_CURRENT
+  CLEAR_CURRENT,
+  SEARCH_LOGS
 } from '../actions/types';
 
 const initialState = {
@@ -57,6 +58,11 @@ export default (state = initialState, action) => {
         logs: state.logs.map(log =>
           log.id === action.payload.id ? action.payload : log
         )
+      };
+    case SEARCH_LOGS:
+      return {
+        ...state,
+        logs: action.payload
       };
     case LOGS_ERROR:
       console.error(action.payload);
